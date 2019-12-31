f8f49195d3c841c6bdf0be0a518bb82709586ec1
pushpa446 <pdumpala@thoughtworks.com>
2019-12-31 14:14:31 +0530

    add new route to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -19,9 +18,4 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
 }

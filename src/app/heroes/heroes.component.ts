6f4d46040f57ac355c87bc6626cae0d0ccff76af
pushpa446 <pdumpala@thoughtworks.com>
2019-12-31 11:02:02 +0530

    add heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 123,
+    name: 'Superman'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
